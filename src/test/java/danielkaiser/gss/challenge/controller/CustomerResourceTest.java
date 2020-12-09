8ee891753a7951bcecb7936e879f2e8e605e7f5c
Daniel Kaiser <danielkaiser@fastmail.com>
2020-12-09 16:51:40 +0100

    Add some more assertions to the test.

--- src/test/java/danielkaiser/gss/challenge/controller/CustomerResourceTest.java
+++ src/test/java/danielkaiser/gss/challenge/controller/CustomerResourceTest.java
@@ -2,12 +2,21 @@ package danielkaiser.gss.challenge.controller;
 
 import danielkaiser.gss.challenge.AbstractTest;
 import danielkaiser.gss.challenge.controller.dto.CustomerCreationDto;
+import danielkaiser.gss.challenge.data.CustomerRepository;
+import danielkaiser.gss.challenge.domain.Customer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasLength;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,6 +24,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class CustomerResourceTest extends AbstractTest {
 
     private static final String URI = "/api/customer";
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Test
     void shouldCreateNewCustomer() throws Exception {
@@ -28,10 +40,21 @@ class CustomerResourceTest extends AbstractTest {
 
         final byte[] inputJson = super.convertObjectToJsonBytes(dto);
 
-        mvc.perform(MockMvcRequestBuilders.post(URI)
-                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andExpect(status().isCreated())
-                .andExpect(jsonPath("$").value(hasLength(8)));
+        final MockHttpServletResponse response =
+                mvc.perform(MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$").value(hasLength(8)))
+                        .andReturn()
+                        .getResponse();
 
+        final String location = response.getHeader("location");
+        assertThat(location).startsWith(URI);
 
+        final List<String> insuranceNumbers = customerRepository.findAll().stream().map(Customer::getInsuranceNumber).collect(Collectors.toUnmodifiableList());
+        final Set<String> uniqueInsuranceNumbers = new HashSet<>(insuranceNumbers);
+
+        // same size means, that the numbers are actually unique already and this is what we verify here
+        assertThat(insuranceNumbers).hasSameSizeAs(uniqueInsuranceNumbers);
     }
 }
