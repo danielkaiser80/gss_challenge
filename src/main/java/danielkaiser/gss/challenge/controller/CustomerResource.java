4069809637ccff33b5d11938ed05f0a8cea18c12
Daniel Kaiser <danielkaiser@fastmail.com>
2020-12-15 00:27:47 +0100

    Finalize swagger documentataion.

--- src/main/java/danielkaiser/gss/challenge/controller/CustomerResource.java
+++ src/main/java/danielkaiser/gss/challenge/controller/CustomerResource.java
@@ -7,6 +7,7 @@ import danielkaiser.gss.challenge.controller.dto.InsuranceNumberDto;
 import danielkaiser.gss.challenge.service.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -36,31 +37,42 @@ import static org.springframework.http.MediaType.*;
 @Log4j2
 public class CustomerResource {
 
+    private static final String CUSTOMER_EXAMPLE = """
+            {
+              "id": 17,
+              "firstName": "Daniel",
+              "lastName": "Kaiser",
+              "insuranceNumber": "58e71225"
+              "dateOfBirth": "1980-08-04",
+              "inceptionOfPolicy": "2010-01-01",
+              "paymentRate": 270
+            }""";
+
     private final CustomerService customerService;
 
     @Operation(summary = "Create customer", description = "Create a new customer with the given information and return his insurance number.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Successful created, insurance number returned.",
-                    content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = InsuranceNumberDto.class), examples = {
-                            @ExampleObject(value = """
-                                    {
-                                      "firstName": "Daniel",
-                                      "lastName": "Kaiser",
-                                      "dateOfBirth": "1980-08-04",
-                                      "inceptionOfPolicy": "2010-01-01"
-                                    }""")
-                    })}
+                    content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = InsuranceNumberDto.class), examples =
+                    @ExampleObject(value = """
+                            {
+                              "firstName": "Daniel",
+                              "lastName": "Kaiser",
+                              "dateOfBirth": "1980-08-04",
+                              "inceptionOfPolicy": "2010-01-01"
+                            }""")
+                    )
             ),
-            @ApiResponse(responseCode = "400", description = "Invalid input.", content = @Content(mediaType = TEXT_PLAIN_VALUE, examples = {@ExampleObject(value = "Input data is not valid.")})),
+            @ApiResponse(responseCode = "400", description = "Invalid input.", content = @Content(mediaType = TEXT_PLAIN_VALUE, examples = @ExampleObject(value = "Input data is not valid."))),
     })
     @PostMapping
     public ResponseEntity<InsuranceNumberDto> createCustomer(
-            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = CustomerCreationDto.class), examples = {
-                    @ExampleObject(value = """
-                            {
-                              "insuranceNumber": "58e71225"
-                            }""")
-            }))
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = CustomerCreationDto.class), examples =
+            @ExampleObject(value = """
+                    {
+                      "insuranceNumber": "58e71225"
+                    }""")
+            ))
             @Valid @RequestBody CustomerCreationDto customerCreationDto) throws URISyntaxException {
         final CustomerCreatedDto result = customerService.createCustomer(customerCreationDto);
         return ResponseEntity.created(new URI("/api/customers/" + result.getId()))
@@ -71,20 +83,9 @@ public class CustomerResource {
     @Operation(summary = "Retrieve a customer", description = "Retrieve a customer using his insurance number.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful retrieved.",
-                    content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = CustomerDto.class), examples = {
-                            @ExampleObject(value = """
-                                    {
-                                      "id": 17,
-                                      "firstName": "Daniel",
-                                      "lastName": "Kaiser",
-                                      "insuranceNumber": "58e71225"
-                                      "dateOfBirth": "1980-08-04",
-                                      "inceptionOfPolicy": "2010-01-01",
-                                      "paymentRate": 270
-                                    }""")
-                    })}
+                    content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = CustomerDto.class), examples = @ExampleObject(value = CUSTOMER_EXAMPLE))
             ),
-            @ApiResponse(responseCode = "404", description = "No customer with the supplied insurance number exists.", content = @Content(mediaType = ALL_VALUE, examples = {@ExampleObject})),
+            @ApiResponse(responseCode = "404", description = "No customer with the supplied insurance number exists.", content = @Content(mediaType = ALL_VALUE, examples = @ExampleObject)),
     })
     @GetMapping(value = "{insuranceNumber}")
     @Transactional(readOnly = true)
@@ -94,11 +95,10 @@ public class CustomerResource {
     }
 
 
-    /**
-     * GET  /api/customers : retrieve all customers with payment information.
-     *
-     * @return 200 and a list of customers with payment rates (maybe empty)
-     */
+    @Operation(summary = "Retrieve all customers", description = "Retrieve all customers with payment information as a list.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation.", content = @Content(array = @ArraySchema(schema = @Schema(implementation = CustomerDto.class, example = CUSTOMER_EXAMPLE))))
+    })
     @GetMapping
     @Transactional(readOnly = true)
     @ResponseBody
--- src/main/java/danielkaiser/gss/challenge/controller/dto/InsuranceNumberDto.java
+++ src/main/java/danielkaiser/gss/challenge/controller/dto/InsuranceNumberDto.java
@@ -1,5 +1,6 @@
 package danielkaiser.gss.challenge.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 import org.hibernate.validator.constraints.Length;
 
@@ -9,5 +10,6 @@ import org.hibernate.validator.constraints.Length;
 @Value(staticConstructor = "of")
 public class InsuranceNumberDto {
     @Length(min = 8, max = 8)
+    @Schema(required = true)
     String insuranceNumber;
 }
